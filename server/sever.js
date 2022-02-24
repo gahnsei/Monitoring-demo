afc0f4ae53944a65a65802aff4bbf6896b3bfdcc
gahnsei <gahnsei@gmail.com>
2022-02-24 13:40:13 -0600

    json parser added

--- server/sever.js
+++ server/sever.js
@@ -3,6 +3,8 @@ const express = require(`express`)
 const path = require(`path`)
 const app = express()
 
+app.use(express.json())
+
 // include and initialize the rollbar library with your access token
 const Rollbar = require('rollbar')
 const rollbar = new Rollbar({

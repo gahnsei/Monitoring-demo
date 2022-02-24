2f865a24f0f45fb679ea1278f5177debbc325214
gahnsei <gahnsei@gmail.com>
2022-02-24 13:12:23 -0600

    rollbar added

--- server/sever.js
+++ server/sever.js
@@ -3,6 +3,17 @@ const express = require(`express`)
 const path = require(`path`)
 const app = express()
 
+// include and initialize the rollbar library with your access token
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+  accessToken: '105f51ff01f54af4af9ca0171e6665c1',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 app.get(`/`, (req, res) => {
     res.sendFile(path.join(__dirname, `../public/index.html`)
 )})
